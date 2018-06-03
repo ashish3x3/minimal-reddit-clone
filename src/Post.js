374c5a4270c5a947809eafda7a9157ecab8ed96b
Ashish Singh <aks133844@gmail.com>
2018-06-04 02:30:42 +0530

    fixing responsive UI issues with small and big screen. Added animation in loading created post

--- src/Post.js
+++ src/Post.js
@@ -3,31 +3,31 @@ import { connect } from 'react-redux';
 class Post extends Component {
 	render() {
 		return (
-			<div className="post">
+			<div className="w3-animate-zoom post">
 				<h2 className="post_title">{this.props.post.title}</h2>
 				<p className="post_message">{this.props.post.message}</p>
 
 				<div className="control-buttons">
-				    <button className="fa fa-pencil-square-o edit"
+				    <button className="fa fa-pencil-square-o post-control-buttons edit"
 				        		onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })}>
 				        	Edit
 				    </button>
 
-				    <button className="fa fa-trash delete"
+				    <button className="fa fa-trash post-control-buttons delete"
 				    		onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>
 						Delete
 					</button>
 
-				    <button className="fa fa-thumbs-up upvote"
+				    <button className="fa fa-thumbs-up post-control-buttons upvote"
 				    		onClick={() => this.props.dispatch({ type: 'UPVOTE', id: this.props.post.id })}>
 						Upvote
 					</button>
 
-				    <button className="count">
+				    <button className="post-control-buttons count">
 						{this.props.post.voteCount}
 					</button>
 
-				    <button className="fa fa-thumbs-down downvote"
+				    <button className="fa fa-thumbs-down post-control-buttons downvote"
 				    		onClick={() => this.props.dispatch({ type: 'DOWNVOTE', id: this.props.post.id })}>
 						Downvote
 					</button>
