12ae1f65b4543ced81bd4ae9340d7ff3ead0011b
Ashish Singh <aks133844@gmail.com>
2018-06-04 01:37:44 +0530

    Added support for upvote and downvote functionalty for any posted content.Added little styling and animation in the way post were geting created and button were getting clicked

--- src/Post.js
+++ src/Post.js
@@ -7,13 +7,29 @@ return (
   <h2 className="post_title">{this.props.post.title}</h2>
   <p className="post_message">{this.props.post.message}</p>
   <div className="control-buttons">
-    <button className="edit"
-    onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })
-    }
-    >Edit</button>
-    <button className="delete"
-    onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}
-    >Delete</button>
+    <button className="fa fa-pencil-square-o edit"
+        		onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })}>
+        	Edit
+        </button>
+
+        <button className="fa fa-trash delete"
+        		onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>
+    		Delete
+    	</button>
+
+        <button className="fa fa-thumbs-up upvote"
+        		onClick={() => this.props.dispatch({ type: 'UPVOTE', id: this.props.post.id })}>
+    		Upvote
+    	</button>
+
+        <button className="count">
+    		{this.props.post.voteCount}
+    	</button>
+
+        <button className="fa fa-thumbs-down downvote"
+        		onClick={() => this.props.dispatch({ type: 'DOWNVOTE', id: this.props.post.id })}>
+    		Downvote
+    	</button>
   </div>
 </div>
 );
