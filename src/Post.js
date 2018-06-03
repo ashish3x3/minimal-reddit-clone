c01b6d9feb93349159e5335e1b13c635938ab9b8
Ashish Singh <aks133844@gmail.com>
2018-06-04 01:53:49 +0530

    Fixing indendation issues which happendd in sublime

--- src/Post.js
+++ src/Post.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 class Post extends Component {
-render() {
-return (
-<div className="post">
-  <h2 className="post_title">{this.props.post.title}</h2>
-  <p className="post_message">{this.props.post.message}</p>
-  <div className="control-buttons">
-    <button className="fa fa-pencil-square-o edit"
-        		onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })}>
-        	Edit
-        </button>
+	render() {
+		return (
+			<div className="post">
+				<h2 className="post_title">{this.props.post.title}</h2>
+				<p className="post_message">{this.props.post.message}</p>
 
-        <button className="fa fa-trash delete"
-        		onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>
-    		Delete
-    	</button>
+				<div className="control-buttons">
+				    <button className="fa fa-pencil-square-o edit"
+				        		onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })}>
+				        	Edit
+				    </button>
 
-        <button className="fa fa-thumbs-up upvote"
-        		onClick={() => this.props.dispatch({ type: 'UPVOTE', id: this.props.post.id })}>
-    		Upvote
-    	</button>
+				    <button className="fa fa-trash delete"
+				    		onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>
+						Delete
+					</button>
 
-        <button className="count">
-    		{this.props.post.voteCount}
-    	</button>
+				    <button className="fa fa-thumbs-up upvote"
+				    		onClick={() => this.props.dispatch({ type: 'UPVOTE', id: this.props.post.id })}>
+						Upvote
+					</button>
 
-        <button className="fa fa-thumbs-down downvote"
-        		onClick={() => this.props.dispatch({ type: 'DOWNVOTE', id: this.props.post.id })}>
-    		Downvote
-    	</button>
-  </div>
-</div>
-);
-}
+				    <button className="count">
+						{this.props.post.voteCount}
+					</button>
+
+				    <button className="fa fa-thumbs-down downvote"
+				    		onClick={() => this.props.dispatch({ type: 'DOWNVOTE', id: this.props.post.id })}>
+						Downvote
+					</button>
+				</div>
+			</div>
+			);
+	}
 }
 export default connect()(Post);
