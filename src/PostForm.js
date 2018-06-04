f5e0ad23832c01dcb5bd7cc9171391b18181f7cc
Ashish Singh <aks133844@gmail.com>
2018-06-05 00:50:28 +0530

    resolved dependency issue with webpack and react. passed failing build because of npm dependency issues. took 4 hours :p

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import {reset} from 'redux-form';
 
 
-class PostForm extends Component {
+export class PostForm extends Component {
+
+	constructor(props) {
+	    super(props);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	    this.onChangeTitle = this.onChangeTitle.bind(this);
+	    this.onChangeContent = this.onChangeContent.bind(this);
+	    this.getTitle;
+	    this.getMessage;
+	  }
+
+
+	onChangeTitle = (e) => {
+		// e.preventDefault();
+		this.getTitle = e.target.value;
+	}
+
+	onChangeContent = (e) => {
+		// e.preventDefault();
+		this.getMessage = e.target.value;
+	}
+
 	handleSubmit = (e) => {
 		e.preventDefault();
-		const title = this.getTitle.value;
-		const message = this.getMessage.value;
+		// const title = this.getTitle.value;
+		// const message = this.getMessage.value;
 		const data = {
 			id: new Date(),
-			title,
-			message,
+			'title':this.getTitle,
+			'message':this.getMessage,
 			editing: false,
 			voteCount: 0
 		}
@@ -20,18 +42,20 @@ class PostForm extends Component {
 			data
 		})
 
-		this.getTitle.value = '';
-		this.getMessage.value = '';
+		/* This has been reset so that after every succesful submit the form should be refreshed to enter new post */
+		// this.getTitle = '';
+		// this.getMessage = '';
+		// this.props.dispatch(reset('create-post-form'));
 	}
 	render() {
 		return (
 		<div className="post-container">
 			<h1 className="post_heading">Create Post</h1>
-			<form className="form" onSubmit={this.handleSubmit} >
-				<input required type="text" ref={(input) => this.getTitle = input}
-						placeholder="Enter Post Title" /><br /><br />
-				<textarea required rows="5" ref={(input) => this.getMessage = input}
-						cols="28" placeholder="Enter Post" /><br /><br />
+			<form name="create-post-form" className="form" onSubmit={this.handleSubmit} >
+				<input name="title" required type="text" ref={(input) => this.getTitle = input}
+						placeholder="Enter Post Title" onChange={this.onChangeTitle} /><br /><br />
+				<textarea name="content" required rows="5" ref={(input) => this.getMessage = input}
+						cols="28" placeholder="Enter Post" onChange={this.onChangeContent} /><br /><br />
 				<button>Post</button>
 			</form>
 		</div>
